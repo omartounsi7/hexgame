0553b62eaf484b44d678f8030d490f19defd33c2
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-09-20 03:54:56 +0100

    Create basic Army class with a firepower field
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>

---
+++ src/com/omar/models/faction/Army.java
@@ -0,0 +1,14 @@
+package com.omar.models.faction;
+
+public class Army {
+    private int firepower;
+    public Army(int firepower) {
+        this.firepower = firepower;
+    }
+    public int getFirepower() {
+        return firepower;
+    }
+    public void setFirepower(int firepower) {
+        this.firepower = firepower;
+    }
+}
