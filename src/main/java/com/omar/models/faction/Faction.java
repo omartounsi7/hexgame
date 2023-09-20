20817505d26b802a976b20dcc8edc25062dcf560
Omar Tounsi <62721212+omartounsi7@users.noreply.github.com>
2023-09-20 19:58:13 +0100

    Refactoring Faction and Army classes (#8)
    * Added position field to Army class
    
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
    
    * Updated the implementation of the armies field from Map to List
    
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
    
    * Add test for initArmies function
    
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>

--- src/main/java/com/omar/models/faction/Faction.java
+++ src/main/java/com/omar/models/faction/Faction.java
@@ -1,31 +1,31 @@
 package com.omar.models.faction;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Faction {
     private final String name;
-    private final Map<Army, Integer> armies;
+    private final List<Army> armies;
     public Faction(String name, int startingPos) {
         this.name = name;
-        this.armies = new HashMap<>();
+        this.armies = new LinkedList<>();
         initArmies(startingPos);
     }
     public String getName() {
         return name;
     }
+    public List<Army> getArmies(){
+        return armies;
+    }
+    public Army getArmy(int index){
+        return armies.get(index);
+    }
     private void initArmies(int startingPos){
-        armies.put(new Army(10), startingPos);
+        armies.add(new Army(10, startingPos));
     }
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("{").append(name).append("}\n");
-        for (Army v : armies.keySet()) {
-            builder.append(v.toString()).append(": ");
-            builder.append(armies.get(v).toString()).append(" ");
-            builder.append("\n");
-        }
-        return (builder.toString());
+        return name + ": Armies -> " + armies;
     }
 }
