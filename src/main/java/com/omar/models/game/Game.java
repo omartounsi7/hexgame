96931fd14eb1a7fdffde926b0b9db91be069c5f9
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-10-10 17:19:42 +0100

    Create play func
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>

--- src/main/java/com/omar/models/game/Game.java
+++ src/main/java/com/omar/models/game/Game.java
@@ -15,6 +15,18 @@ public class Game {
         this.factions = factions;
         this.status = GameStatus.ACTIVE;
     }
+    public void play(){
+        while(status == GameStatus.ACTIVE){
+            status = GameStatus.P2WINS;
+        }
+        if(status == GameStatus.P1WINS){
+            System.out.println(factions[0].getName() + " has won!");
+        } else {
+            System.out.println(factions[1].getName() + " has won!");
+        }
+        System.out.println("Game over.");
+        scanner.close();
+    }
     public World getWorld() {
         return world;
     }
