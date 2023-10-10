adcd1226acd08c66b378763a11bd33a3a375de5e
Omar Tounsi <62721212+omartounsi7@users.noreply.github.com>
2023-10-10 21:07:58 +0100

    Create TileStatus enum
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>

--- src/main/java/com/omar/models/world/Tile.java
+++ src/main/java/com/omar/models/world/Tile.java
@@ -2,11 +2,19 @@ package com.omar.models.world;
 
 public class Tile {
     private final int number;
+    private TileStatus status;
     public Tile(int number) {
         this.number = number;
+        this.status = TileStatus.EMPTY;
+    }
+    public void setStatus(TileStatus status) {
+        this.status = status;
     }
     public int getNumber() {
         return number;
+    }
+    public TileStatus getStatus() {
+        return status;
     }
     @Override
     public String toString() {
---
+++ src/main/java/com/omar/models/world/TileStatus.java
@@ -0,0 +1,5 @@
+package com.omar.models.world;
+
+public enum TileStatus {
+    EMPTY, P1OCCUPIED, P2OCCUPIED;
+}
