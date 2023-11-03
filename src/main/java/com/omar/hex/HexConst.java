d97ba2bb7e99dcdeaa5a67a0990d5b71ac3e95b0
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-11-03 19:03:24 +0100

    Change some colors
    Signed-off-by: omartounsi7 <62721212+omartounsi7@users.noreply.github.com>

--- src/main/java/com/omar/hex/HexConst.java
+++ src/main/java/com/omar/hex/HexConst.java
@@ -6,10 +6,10 @@ public class HexConst {
     final static Color BGCOLOR =  Color.BLACK;
     final static Color EMPTYCOLOR =  Color.ORANGE;
     final static Color GRIDCOLOR =  Color.BLACK;
-    final static Color P1COLOR = new Color( 139, 0, 0);
-    final static Color P1ADJCOLOR = new Color(173, 216, 230);
-    final static Color P2COLOR = new Color( 0, 128, 0);
-    final static Color P2ADJCOLOR = new Color(144, 238, 144);
+    final static Color P1COLOR = new Color( 140, 0, 0);
+    final static Color P1ADJCOLOR = new Color(220,50,50);
+    final static Color P2COLOR = new Color( 0, 140, 0);
+    final static Color P2ADJCOLOR = new Color(50, 240, 50);
     final static Color TXTCOLOR = Color.BLACK;
     final static int MAPSIZE = 8;
     final static int TILESIZE = 60;
